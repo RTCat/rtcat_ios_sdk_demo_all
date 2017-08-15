06f11af14fbdb2a397e4db19f61dcb9374aa7f80
spacetime <jingweiming-sp@outlook.com>
2017-08-15 14:02:17 +0800

    增加 SFU Demo，未完成

---
+++ 7.SFU/7.SFU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  7.SFU
+//
+//  Created by spacetime on 8/15/17.
+//  Copyright © 2017 shishimao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 7.SFU/7.SFU/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  7.SFU
+//
+//  Created by spacetime on 8/15/17.
+//  Copyright © 2017 shishimao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
